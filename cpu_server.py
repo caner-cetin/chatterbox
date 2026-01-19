b8934e15c8557f33d28331911d547fc227387c5d
Caner Cetin <canercetin853@gmail.com>
2026-01-20 02:51:05 +0300

    meow

--- cpu_server.py
+++ cpu_server.py
@@ -250,12 +250,16 @@ class StandaloneApplication(WSGIApplication):
     def __init__(self, app, options=None):
         self.options = options or {}
         self.application = app
+        self.app_uri = None
         super().__init__()
     
     def load_config(self):
         for key, value in self.options.items():
             if key in self.cfg.settings and value is not None:
                 self.cfg.set(key.lower(), value)
+    
+    def load(self):
+        return self.application
     
     def on_starting(self, server):
         logger.info("Gunicorn worker starting...")
